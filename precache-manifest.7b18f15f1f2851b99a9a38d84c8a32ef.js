17350722eb076288a07f5953ef5bba02a93bd92e
Thao Nguyen <ttn@google.com>
2020-01-20 01:12:58 -0500

    Updates

---
+++ precache-manifest.7b18f15f1f2851b99a9a38d84c8a32ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888732184e4eaa12ad8d4a0842f20302",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "4a46396993e935bed30a",
+    "url": "/new-portfolio/static/css/main.f4d8a952.chunk.css"
+  },
+  {
+    "revision": "3f6d8cafcddda0d2ce11",
+    "url": "/new-portfolio/static/js/2.5b4ad04b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/new-portfolio/static/js/2.5b4ad04b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a46396993e935bed30a",
+    "url": "/new-portfolio/static/js/main.3614798c.chunk.js"
+  },
+  {
+    "revision": "0b03270f7b6e9f5a943f",
+    "url": "/new-portfolio/static/js/runtime-main.45fbbe94.js"
+  }
+]);
